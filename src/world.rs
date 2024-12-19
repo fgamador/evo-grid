c04adceb9d2a25149fb8ab8c12a0bcf85da6a374
Franz Amador <franzamador@gmail.com>
2024-12-19 12:19:22 -0800

    Sketching in substance code

--- src/world.rs
+++ src/world.rs
@@ -3,10 +3,6 @@
 
 use array2d::{Array2D /*, Error */};
 
-const BIRTH_RULE: [bool; 9] = [false, false, false, true, false, false, false, false, false];
-const SURVIVE_RULE: [bool; 9] = [false, false, true, true, false, false, false, false, false];
-const INITIAL_FILL: f32 = 0.3;
-
 #[derive(Clone, Debug)]
 pub struct WorldGrid {
     cells: Array2D<GridCell>,
@@ -51,22 +47,15 @@ impl WorldGrid {
         let mut rng: randomize::PCG32 = generate_seed().into();
         for i in 0..self.cells.num_elements() {
             let cell = self.cells.get_mut_row_major(i).unwrap();
-            let alive = randomize::f32_half_open_right(rng.next_u32()) > INITIAL_FILL;
-            *cell = GridCell::new(alive);
-        }
-
-        // run a few simulation iterations for aesthetics (If we don't, the
-        // noise is ugly)
-        for _ in 0..3 {
-            self.update();
+            *cell = GridCell::new(randomize::f32_closed(rng.next_u32()));
         }
     }
 
     pub fn update(&mut self) {
         for row in 0..self.height() {
             for col in 0..self.width() {
-                let num_neighbors = self.count_neighbors(row, col);
-                let next = self.cells[(row, col)].update(num_neighbors);
+                // let num_neighbors = self.count_neighbors(row, col);
+                let next = self.cells[(row, col)].update();
                 // Write into scratch_cells, since we're still reading from `self.cells`
                 self.scratch_cells[(row, col)] = next;
             }
@@ -74,29 +63,29 @@ impl WorldGrid {
         std::mem::swap(&mut self.scratch_cells, &mut self.cells);
     }
 
-    fn count_neighbors(&self, row: usize, col: usize) -> usize {
-        let (col_left, col_right) = neighbor_indexes(col, self.width() - 1);
-        let (row_above, row_below) = neighbor_indexes(row, self.height() - 1);
-        self.cells[(row_above, col_left)].alive as usize
-           + self.cells[(row_above, col)].alive as usize
-           + self.cells[(row_above, col_right)].alive as usize
-           + self.cells[(row, col_left)].alive as usize
-           + self.cells[(row, col_right)].alive as usize
-           + self.cells[(row_below, col_left)].alive as usize
-           + self.cells[(row_below, col)].alive as usize
-           + self.cells[(row_below, col_right)].alive as usize
-    }
+    // fn count_neighbors(&self, row: usize, col: usize) -> usize {
+    //     let (col_left, col_right) = neighbor_indexes(col, self.width() - 1);
+    //     let (row_above, row_below) = neighbor_indexes(row, self.height() - 1);
+    //     self.cells[(row_above, col_left)].alive as usize
+    //        + self.cells[(row_above, col)].alive as usize
+    //        + self.cells[(row_above, col_right)].alive as usize
+    //        + self.cells[(row, col_left)].alive as usize
+    //        + self.cells[(row, col_right)].alive as usize
+    //        + self.cells[(row_below, col_left)].alive as usize
+    //        + self.cells[(row_below, col)].alive as usize
+    //        + self.cells[(row_below, col_right)].alive as usize
+    // }
 }
 
-fn neighbor_indexes(cell_index: usize, max_index: usize) -> (usize, usize) {
-    if cell_index == 0 {
-        (max_index, 1)
-    } else if cell_index == max_index {
-        (max_index - 1, 0)
-    } else {
-        (cell_index - 1, cell_index + 1)
-    }
-}
+// fn neighbor_indexes(cell_index: usize, max_index: usize) -> (usize, usize) {
+//     if cell_index == 0 {
+//         (max_index, 1)
+//     } else if cell_index == max_index {
+//         (max_index - 1, 0)
+//     } else {
+//         (cell_index - 1, cell_index + 1)
+//     }
+// }
 
 /// Generate a pseudorandom seed for the game's PRNG.
 fn generate_seed() -> (u64, u64) {
@@ -115,27 +104,38 @@ fn generate_seed() -> (u64, u64) {
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct GridCell {
-    pub alive: bool,
+    pub substance: Substance,
 }
 
 impl GridCell {
-    fn new(alive: bool) -> Self {
-        Self { alive, }
+    fn new(amount: f32) -> Self {
+        Self {
+            substance: Substance::new(amount),
+        }
     }
 
     #[must_use]
-    fn update(self, num_neighbors: usize) -> Self {
-        let next_alive = if self.alive {
-            SURVIVE_RULE[num_neighbors]
-        } else {
-            BIRTH_RULE[num_neighbors]
-        };
-        self.next_state(next_alive)
+    fn update(self) -> Self {
+        self.next_state()
     }
 
     #[must_use]
-    fn next_state(mut self, alive: bool) -> Self {
-        self.alive = alive;
+    fn next_state(mut self) -> Self {
         self
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Substance {
+    pub color: [u8; 3],
+    pub amount: f32,
+}
+
+impl Substance {
+    fn new(amount: f32) -> Self {
+        Self {
+            color: [0xff, 0, 0],
+            amount,
+        }
     }
 }
