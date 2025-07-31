9f16db6925addbb133ef1ee5122cebb434588b72
fgamador <franzamador@gmail.com>
2025-07-31 12:15:49 -0700

    Experimenting with how to upgrade pixels crate

---
+++ pixels-problem/src/main.rs
@@ -0,0 +1,48 @@
+use pixels::{Pixels, PixelsBuilder, SurfaceTexture};
+use std::sync::Arc;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+struct App {
+    window: Option<Arc<Window>>,
+    pixels: Option<Pixels<'static>>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        ));
+        let surface_texture = SurfaceTexture::new(100, 100, self.window.as_ref().unwrap().clone());
+        self.pixels = Some(
+            PixelsBuilder::new(100, 100, surface_texture)
+                .build()
+                .unwrap(),
+        );
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // Draw using self.pixels...
+                self.pixels.as_ref().unwrap().render().unwrap();
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
